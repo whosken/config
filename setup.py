e7a4f0cf4bd51304e744d3311051d3257c9a2a6c
Ken Hu <whosbacon@gmail.com>
2013-08-13 17:42:00 -0400

    include setup file for pip use

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+import config
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+packages = [
+    'config',
+]
+
+requires = []
+
+setup(
+    name='config',
+    description='Extended YAML and Environment Variable Reader',
+    long_description=open('README.md').read(),
+    author='Soshio',
+    author_email='tech@getsoshio.com',
+    packages=packages,
+    package_data={'': ['LICENSE', 'NOTICE'], 'config': ['*.pem']},
+    package_dir={'config': 'config'},
+    include_package_data=True,
+    install_requires=requires,
+    license=open('LICENSE').read(),
+    zip_safe=False,
+    classifiers=(
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+
+    ),
+)
