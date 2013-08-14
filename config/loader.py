16c63f787717dccd10bfb83218e9b0146e4e6531
Ken Hu <whosbacon@gmail.com>
2013-08-13 20:22:43 -0400

    For #1; allow users to overwrite the default config values with another

--- config/loader.py
+++ config/loader.py
@@ -6,11 +6,16 @@ import os
 import os.path
 import yaml
 
+def load(path=None, overwrite=None):
+    config = load_yaml(path)
+    overwrite = overwrite or os.environ.get('CONFIG')
+    if overwrite:
+        config.update(load_yaml(filename=overwrite))
+    return config
+
 default = 'config.yaml'
-def load(path=None):
-    if not path:
-        filename = os.environ.get('CONFIG') or default
-    path = path or os.path.join('config',filename)
+def load_yaml(path=None, filename=None):
+    path = path or os.path.join('config',filename or default)
     logging.debug('Reading yaml from <{0}>'.format(path))
     with open(path) as file:
         return yaml.load(file.read())
